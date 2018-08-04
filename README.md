e8bfe2f636a91575dc2e3224a2b1dd8d541dccf2
natsuno-sino <natsuno.sino@gmail.com>
2018-08-04 11:38:49 +0900

    first

---
+++ README.md
@@ -0,0 +1,27 @@
+# Discord bot
+
+特定の文字列に対して返事を返すBot
+
+## 導入方法
+
+DISCORD側にBOTユーザを作る。(ぐぐれば出るので省略)
+
+同じディレクトリにtoken.jsonを用意する。
+
+```json:token.json
+{
+  "token" : "ディスコードBOTのアクセストークン"
+}
+```
+
+npm install
+
+```
+npm install
+```
+
+npm start
+
+```
+npm start
+```
---
+++ bot.js
@@ -0,0 +1,66 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const token = require( "./token.json" ).token;
+
+client.on('ready', () => {
+    console.log('ready...');
+});
+
+const dictionary = {
+	'ほげ' : 'ほげほげ',
+	'ふが' : 'ふがふが'
+}
+
+
+client.on('message', message => {
+    
+
+    if (dictionary[message.content]) {
+
+        let channel = message.channel;
+        let author = message.author.username;
+        let reply_text = dictionary[message.content];
+
+        channel.send(reply_text)
+            .catch(console.error);
+        return;
+    }
+    
+    if(message.content === 'スタンプリスト'){
+    
+        let channel = message.channel;
+        let author = message.author.username;
+        let reply_text = Object.keys(dictionary).join(' , ');
+        
+        console.log(dictionary);
+
+        channel.send(reply_text)
+            .catch(console.error);
+        return;
+    }
+    
+
+	if(!message.content.indexOf("スタンプ追加") && message.content.match(/ /gm).length === 2){
+		dictionary[message.content.split(/ /gm)[1]] = message.content.split(/ /gm)[2];
+        let channel = message.channel;
+        channel.send('スタンプを追加したぞ')
+            .catch(console.error);
+        return;
+	}
+    
+
+	if(!message.content.indexOf("スタンプ削除") && message.content.match(/ /gm).length === 1 && dictionary[message.content.split(/ /gm)[1]]){
+		delete dictionary[message.content.split(/ /gm)[1]];
+        let channel = message.channel;
+        channel.send('スタンプを削除したぞ')
+            .catch(console.error);
+        return;
+	}
+
+    
+});
+
+
+client.login(token);
